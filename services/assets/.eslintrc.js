19a1987cc7d5d709c9cf2bb71b76f82cdb795b10
Hanish Rao <hanishrao321@gmail.com>
2023-01-18 02:07:56 +0530

    feat: :art: added helm chart,skaffold,dockerfile,procfile, configs with structure

---
+++ services/assets/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: 'galex',
+    rules: {
+      'no-console': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+  }
